d18af8455645ae3c383b5f369e242f15207b7ad6
shuwenchen17 <61525890+shuwenchen17@users.noreply.github.com>
2020-04-24 10:00:24 +0800

    Add files via upload

---
+++ week7/IDS_20200417_2.py
@@ -0,0 +1,7 @@
+monthly_income = input("請輸入月薪：")
+saving_account = input("請輸入存款：")
+ans = "發放信用卡"
+monthly_income = float(monthly_income)
+saving_account  = float(saving_account)
+if monthly_income > 40000 or  saving_account > 500000 :
+ print(ans)
